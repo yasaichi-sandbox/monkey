be15376555891352d24840e0634107183413635c
yasaichi <yasaichi@users.noreply.github.com>
2018-07-04 21:23:08 +0900

    2.6.4

--- ast/ast.go
+++ ast/ast.go
@@ -1,10 +1,12 @@
 package ast
 
 import (
+	"bytes"
 	"github.com/yasaichi-sandbox/monkey/token"
 )
 
 type Node interface {
+	String() string
 	TokenLiteral() string
 }
 
@@ -24,6 +26,16 @@ type Program struct {
 	Statements []Statement
 }
 
+func (p *Program) String() string {
+	var out bytes.Buffer
+
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+
+	return out.String()
+}
+
 func (p *Program) TokenLiteral() string {
 	if len(p.Statements) > 0 {
 		return p.Statements[0].TokenLiteral()
@@ -37,10 +49,35 @@ type Identifier struct {
 	Value string
 }
 
-func (*Identifier) expressionNode() {}
+func (i *Identifier) String() string {
+	// NOTE: 今のところ`i.Token.Literal`との違いがわかっていない
+	return i.Value
+}
+
 func (i *Identifier) TokenLiteral() string {
 	return i.Token.Literal
 }
+
+func (*Identifier) expressionNode() {}
+
+type ExpressionStatement struct {
+	Token      token.Token
+	Expression Expression
+}
+
+func (es *ExpressionStatement) String() string {
+	if es.Expression != nil {
+		es.Expression.String()
+	}
+
+	return ""
+}
+
+func (es *ExpressionStatement) TokenLiteral() string {
+	return es.Token.Literal
+}
+
+func (*ExpressionStatement) statementNode() {}
 
 type LetStatement struct {
 	Token token.Token
@@ -48,17 +85,50 @@ type LetStatement struct {
 	Value Expression
 }
 
-func (*LetStatement) statementNode() {}
+func (ls *LetStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(ls.TokenLiteral() + " ")
+	out.WriteString(ls.Name.String())
+	out.WriteString(" = ")
+
+	// NOTE: ここでnilだった場合でも`= ;`は出力されるのめちゃくちゃ微妙な気がするんだけどどうなんだろう
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+}
+
 func (ls *LetStatement) TokenLiteral() string {
 	return ls.Token.Literal
 }
+
+func (*LetStatement) statementNode() {}
 
 type ReturnStatement struct {
 	Token       token.Token
 	ReturnValue Expression
 }
 
+func (rs *ReturnStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(rs.TokenLiteral() + " ")
+
+	if rs.ReturnValue != nil {
+		out.WriteString(rs.ReturnValue.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+}
+
 func (rs *ReturnStatement) TokenLiteral() string {
 	return rs.Token.Literal
 }
+
 func (*ReturnStatement) statementNode() {}
---
+++ ast/ast_test.go
@@ -0,0 +1,41 @@
+package ast_test
+
+import (
+	"github.com/yasaichi-sandbox/monkey/ast"
+	"github.com/yasaichi-sandbox/monkey/token"
+	"testing"
+)
+
+func TestString(t *testing.T) {
+	program := ast.Program{
+		Statements: []ast.Statement{
+			&ast.LetStatement{
+				Token: token.Token{
+					Type:    token.LET,
+					Literal: "let",
+				},
+				Name: &ast.Identifier{
+					Token: token.Token{
+						Type:    token.IDENT,
+						Literal: "myVar",
+					},
+					Value: "myVar",
+				},
+				Value: &ast.Identifier{
+					Token: token.Token{
+						Type:    token.IDENT,
+						Literal: "anotherVar",
+					},
+					Value: "anotherVar",
+				},
+			},
+		},
+	}
+
+	if program.String() != "let myVar = anotherVar;" {
+		t.Errorf(
+			"program.String() wrong. got=%q",
+			program.String(),
+		)
+	}
+}
