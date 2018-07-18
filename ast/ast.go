c8557d4d42e780f71e34b627a68f8664fc45738a
yasaichi <yasaichi@users.noreply.github.com>
2018-07-18 23:04:00 +0900

    2.8.3 Part 1 (Test failed)

--- ast/ast.go
+++ ast/ast.go
@@ -44,6 +44,27 @@ func (p *Program) TokenLiteral() string {
 	}
 }
 
+type BlockStatement struct {
+	Token      token.Token
+	Statements []Statement
+}
+
+func (bs *BlockStatement) String() string {
+	var out bytes.Buffer
+
+	for _, s := range bs.Statements {
+		out.WriteString(s.String())
+	}
+
+	return out.String()
+}
+
+func (bs *BlockStatement) TokenLiteral() string {
+	return bs.Token.Literal
+}
+
+func (*BlockStatement) statementNode() {}
+
 type ExpressionStatement struct {
 	Token      token.Token
 	Expression Expression
@@ -147,6 +168,35 @@ func (i *Identifier) TokenLiteral() string {
 }
 
 func (*Identifier) expressionNode() {}
+
+type IfExpression struct {
+	Token       token.Token
+	Condition   Expression
+	Consequence *BlockStatement
+	Alternative *BlockStatement
+}
+
+func (ie *IfExpression) String() string {
+	var out bytes.Buffer
+
+	out.WriteString("if")
+	out.WriteString(ie.Condition.String())
+	out.WriteString(" ")
+	out.WriteString(ie.Consequence.String())
+
+	if ie.Alternative != nil {
+		out.WriteString("else")
+		out.WriteString(ie.Alternative.String())
+	}
+
+	return out.String()
+}
+
+func (ie *IfExpression) TokenLiteral() string {
+	return ie.Token.Literal
+}
+
+func (*IfExpression) expressionNode() {}
 
 type InfixExpression struct {
 	Token    token.Token // 演算子トークン、例えば「+」
