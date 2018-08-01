bff2528fe89465b71c79caa6053ca32f9022c487
yasaichi <yasaichi@users.noreply.github.com>
2018-08-01 21:46:33 +0900

    2.9

--- repl/repl.go
+++ repl/repl.go
@@ -4,7 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"github.com/yasaichi-sandbox/monkey/lexer"
-	"github.com/yasaichi-sandbox/monkey/token"
+	"github.com/yasaichi-sandbox/monkey/parser"
 	"io"
 )
 
@@ -12,17 +12,47 @@ const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
-	fmt.Print(PROMPT)
 
-	for scanner.Scan() {
-		line := scanner.Text()
-		l := lexer.New(line)
-
-		// NOTE: 最初わからなかったけど、`i := 0; i < n; i++`などと同じ構造をしている
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+	for {
+		fmt.Printf(PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
 		}
 
-		fmt.Print(PROMPT)
+		line := scanner.Text()
+		l := lexer.New(line)
+		p := parser.New(l)
+
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
+		}
+
+		fmt.Fprintln(out, program.String())
+	}
+}
+
+const MONKEY_FACE = `            __,__
+   .--.  .-"     "-.  .--.
+  / .. \/  .-. .-.  \/ .. \
+ | |  '|  /   Y   \  |'  | |
+ | \   \  \ 0 | 0 /  /   / |
+  \ '- ,\.-"""""""-./, -' /
+   ''-' /_   ^ ^   _\ '-''
+       |  \._   _./  |
+       \   \ '~' /   /
+        '._ '-=-' _.'
+           '-----'
+`
+
+func printParserErrors(out io.Writer, errors []string) {
+	fmt.Fprintln(out, MONKEY_FACE)
+	fmt.Fprintln(out, "Woops! We ran into some monkey business here!")
+	fmt.Fprintln(out, "parser errors:")
+
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
