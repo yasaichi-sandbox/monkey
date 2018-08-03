5688fe3d8fd9419afa290407b3f6ec839b8a53dd
yasaichi <yasaichi@users.noreply.github.com>
2018-08-03 22:08:58 +0900

    3.4.2

--- object/object.go
+++ object/object.go
@@ -1,8 +1,23 @@
 package object
 
+import (
+	"fmt"
+)
+
 type ObjectType string
+
+const (
+	INTEGER_OBJ = "INTEGER"
+)
 
 type Object interface {
 	Type() ObjectType
 	Inspect() string
 }
+
+type Integer struct {
+	Value int64
+}
+
+func (i *Integer) Inspect() string { return fmt.Sprintf("%d", i.Value) }
+func (*Integer) Type() ObjectType  { return INTEGER_OBJ }
