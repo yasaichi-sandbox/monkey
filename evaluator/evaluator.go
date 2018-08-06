2f83c8bb30ea95767c51f103fdc6dd346d71ed81
yasaichi <yasaichi@users.noreply.github.com>
2018-08-06 21:31:17 +0900

    3.5.5

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -19,6 +19,9 @@ func Eval(node ast.Node) object.Object {
 		return evalStatements(node.Statements)
 	case *ast.ExpressionStatement:
 		return Eval(node.Expression)
+	case *ast.PrefixExpression:
+		right := Eval(node.Right)
+		return evalPrefixExpression(node.Operator, right)
 	case *ast.Boolean:
 		return nativeBoolToBooleanObject(node.Value)
 	case *ast.IntegerLiteral:
@@ -26,6 +29,40 @@ func Eval(node ast.Node) object.Object {
 	}
 
 	return nil
+}
+
+func evalBangOperatorExpression(right object.Object) object.Object {
+	switch right {
+	case TRUE:
+		return FALSE
+	case FALSE:
+		return TRUE
+	case NULL:
+		return TRUE
+	}
+
+	// NOTE: truthy values
+	return FALSE
+}
+
+func evalMinusPrefixOperatorExpression(right object.Object) object.Object {
+	if right.Type() != object.INTEGER_OBJ {
+		return NULL
+	}
+
+	value := right.(*object.Integer).Value
+	return &object.Integer{Value: -value}
+}
+
+func evalPrefixExpression(operator string, right object.Object) object.Object {
+	switch operator {
+	case "!":
+		return evalBangOperatorExpression(right)
+	case "-":
+		return evalMinusPrefixOperatorExpression(right)
+	}
+
+	return NULL // NOTE: エラー処理を実装したらNULLを返す以外の実装になるかもしれない
 }
 
 func evalStatements(stmts []ast.Statement) object.Object {
